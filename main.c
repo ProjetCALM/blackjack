9c81d30b1d266483c8a3f8144886fe5b753bea3c
maximGauthier <34715191+maximGauthier@users.noreply.github.com>
2018-01-10 12:57:39 +0100

    Update main.c

--- main.c
+++ main.c
@@ -30,6 +30,7 @@ void jack(){
     int i,j=0;
     int end=0;
     int max=0;
+    int ia=0;
     
     int n=0;
     int tour;   // = nombre de carte tirées
@@ -37,11 +38,11 @@ void jack(){
     carte c;
     srand(time(0));
     //aleaCarte(tour,joueur);
-    
-    
-    
     bjack tab[(PLAYER)];
     tab[10].count = 0;
+    tab[10].stop = 0;
+    
+    
     for(i=0;i<PLAYER;i++){
         tab[i].count = 0;
         tab[i].done = 0;
@@ -51,20 +52,30 @@ void jack(){
             tab[i].jeu.card[j].valeur=0;
     }
     } 
-   
-    n=intPlayer(tab);
     
+    n=intPlayer(tab);
+    ia = tab[0].stop ;
     end = Alpha(n, tab);    
     
     if (end==0){
         Beta(n, tab,0); //tour 2
         Beta(n, tab,1); //tour 3
         Beta(n, tab,2); //tour 4
+        Beta(n, tab,3); //tour 5
 }
     
+    
+    
+    if(tab[10].stop == 0){
     bank(tab);
+    }
+    if(tab[10].stop == 0){
     max = maxCard(n,tab);
+    }if(tab[10].stop == 0){
     printf("\n\nLe plus gros score des joueurs est %d",max);
     compare(max,n,tab);
+    }
     
 }
+
+
